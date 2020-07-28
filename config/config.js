47d0e79111132b34d67888196ac0ee0d2ec2255f
feiy <feiy@163.com>
2020-07-28 16:43:35 +0800

    账号

--- config/config.js
+++ config/config.js
@@ -19,8 +19,8 @@ module.exports = {
         expiresIn: 60*60*24
     },
     wx: {
-        appID: 'wxec99ce1d7b61c46b',
-        appSecret: '3d50d71ebc7e07c36018ae6134fafc69',
+        appID: 'wx7e118972d418de54',
+        appSecret: '5e04d7b099a4f32e93eafa918f424d96',
         loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
     }
 }
